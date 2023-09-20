fa7aa3bc4fa00ebbbc97a99ad9463ea55f9d727c
mcalvocarrera <miguel.calvo.carrera@hotmail.com>
2023-09-20 11:09:51 -0400

    Tests 2

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,3 +29,14 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+def test_daily_min_integers():
+    """Test that mean function works for an array of positive integers."""
+    from inflammation.models import daily_min
+
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([1, 2])
+
+    # Need to use Numpy testing functions to compare arrays
+    npt.assert_array_equal(daily_min(test_input), test_result)
